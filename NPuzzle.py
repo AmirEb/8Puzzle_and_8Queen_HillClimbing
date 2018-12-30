f6a8a3e4e59ae633adb6f4376e78e3c1ec8a8c08
Amir Ebrahimian <44034763+AmirEb@users.noreply.github.com>
2018-12-31 01:01:06 +0330

    Update NPuzzle.py

--- NPuzzle.py
+++ NPuzzle.py
@@ -1,137 +1,246 @@
+import search
 import random
-from copy import deepcopy, copy
-from enum import Enum
-import math
 
-class Directions(Enum):
-    up = 1,
-    down = 2,
-    left = 3,
-    right = 4
+# Module Classes
 
-class NPuzzleProblem:
+class EightPuzzleState:
+    """
+    This class defines the mechanics of the puzzle
+    The task of recasting this puzzle as a search problem is left to
+    the EightPuzzleSearchProblem class.
+    """
 
-    puzzles_width = 3
-    goal = []
-    board = []
+    def __init__( self, numbers ):
 
-    def __init__(self, n):
-        self.puzzles_width = int(math.log(n, 2))
-        self.setGoal()
-        self.setBoard(self.goal)
-
-    # board setter
-    def setBoard(self, board):
-        self.board = board
-    
-    def isGoal(self):
-        return self.board == self.goal
-
-    def getState(self):
-        return self.board
-
-    # setting the problem goal 
-    def setGoal(self):
-        self.goal = []
-        value = 1
-        for i in range(self.puzzles_width):
-            temp = []
-            for j in range(self.puzzles_width):
-                if(value == math.pow(self.puzzles_width, 2)):
-                    value = 0
-                temp.append(value)
-                value += 1
-            self.goal.append(temp)  
-
-    # getting the best successor for a given problem
-    def getSuccessors(self):
-        successors = []
-        for direction in Directions:
-            successor = self.move(direction)
-            if len(successor.board) > 0:
-                successorH = successor.heuristic()
-                successors.append((successor, successorH, [direction.name]))
-        return sorted(successors, key = lambda x: x[1])
-
-    def move(self, direction):
-        x, y = self.getIndex(self.board, 0)
-        next = NPuzzleProblem(int(math.pow(2, self.puzzles_width)))
-        next.setBoard(deepcopy(self.board))
-        blockDirections = self.getBlockDirections((x, y))
-
-        # checking the what directions are blocked
-        if direction in blockDirections:
-            next.setBoard([])
-            return next
-
-        # calculate next position after doing directions
-        x2, y2 = self.getTargetState((x, y), direction)
-
-        # swap
-        target = next.board[x2][y2]
-        next.board[x2][y2] = 0
-        next.board[x][y] = target
-
-        return next
-
-    # calculating block directions of a given state
-    def getBlockDirections(self, state):
-        x, y = state
-        blockDirections = []
-        if x == 0:
-            blockDirections.append(Directions.up)
-        elif x == self.puzzles_width - 1:
-            blockDirections.append(Directions.down)
-        if y == 0:
-            blockDirections.append(Directions.left)
-        elif y == self.puzzles_width - 1:
-            blockDirections.append(Directions.right)
-
-        return blockDirections
-
-    # calculate target state after a given state move towards a given direction
-    def getTargetState(self, state, direction):
-        x, y = state
-        if(direction == Directions.up):
-            x2, y2 = x - 1, y
-        elif(direction == Directions.down):
-            x2, y2 = x + 1, y
-        elif(direction == Directions.left):
-            x2, y2 = x, y - 1
-        elif(direction == Directions.right):
-            x2, y2 = x, y + 1
         
-        return (x2, y2)
+        self.cells = []
+        numbers = numbers[:] # Make a copy so as not to cause side-effects.
+        numbers.reverse()
+        for row in range( 3 ):
+            self.cells.append( [] )
+            for col in range( 3 ):
+                self.cells[row].append( numbers.pop() )
+                if self.cells[row][col] == 0:
+                    self.blankLocation = row, col
 
-    # find a value's position in board
-    def getIndex(self, board,value):
-        for i in range(len(self.board)):
-            for j in range(len(self.board[i])):
-                if board[i][j] == value:
-                    return (i, j)
-    
-    # manhatan heuristic: dx + dy
+    def isGoal( self ):
+
+        current = 0
+        for row in range( 3 ):
+            for col in range( 3 ):
+                if current != self.cells[row][col]:
+                    return False
+                current += 1
+        return True
+
+
+    def findIndex(self, number):
+        """
+            Finds indeces of the element equal to 'number' in the puzzle cells
+        """
+        for row in range( 3 ):
+            for column in range( 3 ):
+                if self.cells[row][column] == number:
+                    return [row,column]
+           
+
     def heuristic(self):
+        """
+            h = sum of manhatan distance for every tile
+        """
         h = 0
-        for k in range(1, int(math.pow(self.puzzles_width, 2))):
-            x1, y1 = self.getIndex(self.board, k)
-            x2, y2 = self.getIndex(self.goal, k)
-            h += math.fabs(x2 - x1) + math.fabs(y2 - y1)
+        current = 0
+        for row in range( 3 ):
+            for column in range( 3 ):
+                #current is the value of goal[row][column]
+                [cell_row , cell_column] = self.findIndex(current)
+                h += abs(cell_row - row) + abs(cell_column - column)
+            current += 1
+        #print('heuristic = ',h)
         return h
 
-    # changes the problems board to a random solvable board
-    def generateRandomBoard(self, hardrate):
-        problem = self
-        problem.board = self.goal
-        directionsList = []
-        for d in Directions:
-            directionsList.append(d)
+    """
+    def heuristic( self ):
         
-        for i in range(hardrate):
-            r = random.randrange(0, 4)
-            temp = problem.move(directionsList[r])
-            while len(temp.board) == 0:
-                r = random.randrange(0, 4)
-                temp = problem.move(directionsList[r])
-            problem = temp
-        self.setBoard(problem.board)
+            #h = tiles out of place
+            #this heuristic didn't work well
+        
+        h = 0;
+        current = 0
+        for row in range( 3 ):
+            for col in range( 3 ):
+                if current == self.cells[row][col]:
+                    h = h + 1
+                current += 1
+        return h
+    """
+
+ 
+    
+    def legalMoves( self ):
+        """
+          Returns a list of legal moves from the current state.
+        """
+        moves = []
+        row, col = self.blankLocation
+        if(row != 0):
+            moves.append('up')
+        if(row != 2):
+            moves.append('down')
+        if(col != 0):
+            moves.append('left')
+        if(col != 2):
+            moves.append('right')
+        return moves
+
+    def result(self, move):
+        """
+          Returns a new eightPuzzle with the current state and blankLocation
+        updated based on the provided move.
+        """
+        row, col = self.blankLocation
+        if(move == 'up'):
+            newrow = row - 1
+            newcol = col
+        elif(move == 'down'):
+            newrow = row + 1
+            newcol = col
+        elif(move == 'left'):
+            newrow = row
+            newcol = col - 1
+        elif(move == 'right'):
+            newrow = row
+            newcol = col + 1
+        else:
+            raise "Illegal Move"
+
+        # Create a copy of the current eightPuzzle
+        newPuzzle = EightPuzzleState([0, 0, 0, 0, 0, 0, 0, 0, 0])
+        newPuzzle.cells = [values[:] for values in self.cells]
+        # And update it to reflect the move
+        newPuzzle.cells[row][col] = self.cells[newrow][newcol]
+        newPuzzle.cells[newrow][newcol] = self.cells[row][col]
+        newPuzzle.blankLocation = newrow, newcol
+
+        return newPuzzle
+
+    # Utilities for comparison and display
+    def __eq__(self, other):
+        """
+            Overloads '==' such that two eightPuzzles with the same configuration
+          are equal.
+        """
+        for row in range( 3 ):
+            if self.cells[row] != other.cells[row]:
+                return False
+        return True
+
+    def __hash__(self):
+        return hash(str(self.cells))
+
+    def __getAsciiString(self):
+        """
+          Returns a display string for the maze
+        """
+        lines = []
+        horizontalLine = ('-' * (13))
+        lines.append(horizontalLine)
+        for row in self.cells:
+            rowLine = '|'
+            for col in row:
+                if col == 0:
+                    col = ' '
+                rowLine = rowLine + ' ' + col.__str__() + ' |'
+            lines.append(rowLine)
+            lines.append(horizontalLine)
+        return '\n'.join(lines)
+
+    def __str__(self):
+        return self.__getAsciiString()
+
+
+class EightPuzzleSearchProblem(search.SearchProblem):
+    """
+      Implementation of a SearchProblem for the  Eight Puzzle domain
+      Each state is represented by an instance of an eightPuzzle.
+    """
+    def __init__(self,puzzle):
+        "Creates a new EightPuzzleSearchProblem which stores search information."
+        self.puzzle = puzzle
+
+    def getStartState(self):
+        return puzzle
+
+    def isGoalState(self,state):
+        return state.isGoal()
+
+    def getSuccessors(self,state):
+        """
+          Returns list of (successor, action, stepCost) pairs where
+          each succesor is either left, right, up, or down
+          from the original state and the cost is 1.0 for each
+        """
+        succ = []
+        for a in state.legalMoves():
+            succ.append((state.result(a), a, 1))
+        return succ
+
+    def getCostOfActions(self, actions):
+        """
+         actions: A list of actions to take
+        This method returns the total cost of a particular sequence of actions.  The sequence must
+        be composed of legal moves
+        """
+        return len(actions)
+    
+
+EIGHT_PUZZLE_DATA = [[1, 0, 2, 3, 4, 5, 6, 7, 8],
+                     [1, 7, 8, 2, 3, 4, 5, 6, 0],
+                     [4, 3, 2, 7, 0, 5, 1, 6, 8],
+                     [5, 1, 3, 4, 0, 2, 6, 7, 8],
+                     [1, 2, 5, 7, 6, 8, 0, 4, 3],
+                     [0, 3, 1, 6, 8, 2, 7, 5, 4]]
+
+def loadEightPuzzle(puzzleNumber):
+
+    return EightPuzzleState(EIGHT_PUZZLE_DATA[puzzleNumber])
+
+def createRandomEightPuzzle(moves=100):
+    """
+      moves: number of random moves to apply
+      Creates a random eight puzzle by applying
+      a series of 'moves' random moves to a solved
+      puzzle.
+    """
+    puzzle = EightPuzzleState([0,1,2,3,4,5,6,7,8])
+    for i in range(moves):
+        # Execute a random legal move
+        puzzle = puzzle.result(random.sample(puzzle.legalMoves(), 1)[0])
+    return puzzle
+
+if __name__ == '__main__':
+
+	for i in range(1,13):
+	    puzzle = createRandomEightPuzzle(25)
+	    print('random puzzle number %d :' % i)
+	    print(puzzle)
+
+	    problem = EightPuzzleSearchProblem(puzzle)
+	    path1 = search.aStarSearch(problem)
+	    path2 = search.hillClimbing(problem)
+	    path3 = search.simulatedAnnealing(problem)
+	    print('A* found a path of %d moves: %s' % (len(path1), str(path1)))
+	    print('hill climbing found a path of %d moves: %s' % (len(path2), str(path2)))
+	    print('simulated Annealing found a path of %d moves: %s' % (len(path3), str(path3)))
+	    print('\n\n\n')
+	"""this was for seeing puzzle for every move, not needed anymore
+	    curr = puzzle
+	    i = 1
+	    for a in path2:
+	        curr = curr.result(a)
+	        print('After %d move%s: %s' % (i, ("", "s")[i>1], a))
+	        print(curr)
+	        raw_input("Press return for the next state...")   # wait for key stroke
+	        i += 1
+	
+	"""
